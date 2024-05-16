87c7bb4d83fffc4df6e11022be806a67c7c5c794
cminshin <158798407+cminshin@users.noreply.github.com>
2024-05-16 13:53:40 +0900

    프런트 코드에서 필요한 부분만 업로드 하기 위한 삭제

--- ai_assistent_app/android/app/src/main/kotlin/com/example/ai_assistent_app/MainActivity.kt
+++
@@ -1,5 +0,0 @@
-package com.example.ai_assistent_app
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity()
--- ai_assistent_app/lib/logs/make_logs.dart
+++
@@ -1,6 +0,0 @@
-import 'dart:io';
-
-void saveLogToFile(String err) async {
-  final file = File('show_logs.txt');
-  await file.writeAsString(err, mode: FileMode.append);
-}
